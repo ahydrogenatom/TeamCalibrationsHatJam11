460c48a20789232a06071cfd74db02adfa71621a
Nick Taylor <gd45nick@vfs.local>
2016-11-18 20:52:13 -0800

    updated project folder structure

---
+++ Assets/Scripts/MovingCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCharacter : MonoBehaviour {
+
+    public int moveSpeed;
+    public int lightRange;
+    public int jumpHeight;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

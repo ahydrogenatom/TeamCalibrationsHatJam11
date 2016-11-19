1b74736c79f732d532634d98bb4cb52f3108b73f
Greg Tinga <gd45greg@vfs.local>
2016-11-19 03:52:38 -0800

    shandeller

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class MovingPlatform : MonoBehaviour {
 
+    private Vector3 [] movements;
+    public float moveDuration;
+
 	// Use this for initialization
 	void Start () {
-	
+        movements = new Vector3[] { new Vector3(0, 0, 0), new Vector3(1, -1, 0), new Vector3(2,-1.2f, 0), new Vector3(3, -1, 0), new Vector3(4, 0, 0)};
+        Tweener moveTween = transform.DOLocalPath(movements, moveDuration);
+        moveTween.SetLoops(-1, LoopType.Yoyo);
+
+        moveTween.SetRelative(true);
+
+        moveTween.SetEase(Ease.InOutCubic);
 	}
 	
 	// Update is called once per frame
